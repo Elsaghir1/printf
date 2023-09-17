59257da431c2063fa29c3803c9587b752bf016ab
Adham <adhamelsa8er@gmail.com>
2023-09-17 00:09:18 -0700

    a

---
+++ _put.c
@@ -0,0 +1,64 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string with newline
+ * @str: the string to print
+ *
+ * Return: void
+ */
+int _puts(char *str)
+{
+	char *a = str;
+
+	while (*str)
+		_putchar(*str++);
+	return (str - a);
+}
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(int c)
+{
+	static int i;
+	static char buf[OUTPUT_BUF_SIZE];
+
+	if (c == BUF_FLUSH || i >= OUTPUT_BUF_SIZE)
+	{
+		write(1, buf, i);
+		i = 0;
+	}
+	if (c != BUF_FLUSH)
+		buf[i++] = c;
+	return (1);
+}
+
+/**
+ * _isdigit - checks if character is digit
+ * @c: the character to check
+ *
+ * Return: 1 if digit, 0 otherwise
+ */
+int _isdigit(int c)
+{
+	return (c >= '0' && c <= '9');
+}
+
+/**
+ * _strlen - returns the length of a string
+ * @s: the string whose length to check
+ *
+ * Return: integer length of string
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (*s++)
+		i++;
+	return (i);
+}

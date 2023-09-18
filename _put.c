8cd8ed20b3610ee570ee12f59487b861fe546a8e
Adham <adhamelsa8er@gmail.com>
2023-09-18 11:07:03 -0700

    a

--- _put.c
+++ _put.c
@@ -36,29 +36,3 @@ int _putchar(int c)
 		buf[i++] = c;
 	return (1);
 }
-
-/**
- * _isdigit - checks if character is digit
- * @c: the character to check
- *
- * Return: 1 if digit, 0 otherwise
- */
-int _isdigit(int c)
-{
-	return (c >= '0' && c <= '9');
-}
-
-/**
- * _strlen - returns the length of a string
- * @s: the string whose length to check
- *
- * Return: integer length of string
- */
-int _strlen(char *s)
-{
-	int i = 0;
-
-	while (*s++)
-		i++;
-	return (i);
-}
